69f2633a137a601f4fffff5ad9d08c5e78aea1c6
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2026-06-30 19:59:02 +0200

    python 3.8 compatible (#3)

--- ament_clang_tidy/ament_clang_tidy/main.py
+++ ament_clang_tidy/ament_clang_tidy/main.py
@@ -24,7 +24,7 @@ import re
 import subprocess
 import sys
 import time
-from typing import Literal
+from typing import List, Literal
 
 from xml.sax.saxutils import escape
 from xml.sax.saxutils import quoteattr
@@ -32,7 +32,7 @@ from xml.sax.saxutils import quoteattr
 import yaml
 
 
-def main(argv: list[str] = sys.argv[1:]) -> Literal[None, 1]:
+def main(argv: List[str] = sys.argv[1:]) -> Literal[None, 1]:
     extensions = ['c', 'cc', 'cpp', 'cxx', 'h', 'hh', 'hpp', 'hxx']
 
     parser = argparse.ArgumentParser(
